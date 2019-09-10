1c1392f34f8e7612eec3a455a12fc77163abf2f5
Ian Dahlberg <ijdahlberg@gmail.com>
2019-09-09 21:07:59 -0700

    added header and start of reg page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Route, Switch, Link } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 
 import './App.css';
 
 import Homepage from "./pages/homepage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
+import SignInAndSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component"
+
+import Header from "./components/header/header.component"
 
 function App() {
   return (
 		<div>
+			<Header/>
 			<Switch>
 				<Route exact path="/" component={Homepage}/>
 				<Route path="/shop" component={ShopPage}/>
+				<Route path="/signinsignup" component={SignInAndSignUp}/>
 			</Switch>
+
 		</div>
   );
 }
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,7 +5,10 @@ import "./collection-item.styles.scss"
 const CollectionItem = ({name, price, imageUrl})=>{
 	return(
 		<div className="collection-item">
-				<div className="image" style={{backgroundImage : `url(${imageUrl})`}}></div>
+			<div 
+				className="image" 
+				style={{backgroundImage : `url(${imageUrl})`}}
+			/>
 				<div className="collection-footer">
 						<div className="name">{name}</div>
 						<div className="price">${price}</div>
