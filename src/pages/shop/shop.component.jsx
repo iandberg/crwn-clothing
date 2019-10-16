d7cf5f9a6dd28db32ee030ec827bc226df3654c1
Ian Dahlberg <ijdahlberg@gmail.com>
2019-10-15 19:56:49 -0700

    added Redux Sagas

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Route } from 'react-router-dom'
 
 import { connect } from 'react-redux'
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
 // pages/components
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
@@ -14,8 +14,8 @@ import CollectionPageContainer from '../collection/collection.container'
 class ShopPage extends React.Component{
 
 	componentDidMount(){
-		const { fetchCollectionsStartAsync } = this.props
-		fetchCollectionsStartAsync()
+		const { fetchCollectionsStart } = this.props
+		fetchCollectionsStart()
 	}
 
 	render(){
@@ -41,7 +41,7 @@ class ShopPage extends React.Component{
 }
 
 const mapDispatchToProps = dispatch => ({
-	fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+	fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,7 +1,5 @@
 import ShopActionTypes from './shop.types'
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
-
 export const fetchCollectionsStart = () => ({
 	type: ShopActionTypes.FETCH_COLLECTIONS_START
 })
@@ -16,21 +14,3 @@ export const fetchCollectionsFailure = errorMessage => ({
 	payload: errorMessage
 })
 
-// thunk in action: we return a function that will be invoked with dispatch as 1st arg
-// we can then dispatch multiple actions within
-export const fetchCollectionsStartAsync = () => {
-
-	return dispatch => {
-		
-		const collectionRef = firestore.collection('collections')
-		dispatch(fetchCollectionsStart())
-
-		collectionRef
-			.get()
-			.then(snapshot =>{
-				const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-				dispatch(fetchCollectionsSuccess(collectionsMap))
-			})
-			.catch(error => dispatch(fetchCollectionsFailure(error.message)))
-	}
-}
