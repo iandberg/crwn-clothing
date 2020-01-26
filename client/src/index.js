dca8223eb7890fd10288592d6c970dd779ee5dbb
Ian Dahlberg <ijdahlberg@gmail.com>
2020-01-26 08:53:03 -0800

    added code to be pwa compliant, with serviceworker, https, manifest

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import './index.css';
 import App from './App';
 
 import { store, persistor } from './redux/store'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -23,3 +24,4 @@ ReactDOM.render(
 	, document.getElementById('root')
 );
 
+serviceWorker.register(); //is expecting service-worker.js file
